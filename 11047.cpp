9da81ae0d504c5e2944d8d0a8608d9a5aaf6e4fd
0318asd <0318asd@naver.com>
2019-07-07 15:37:26 +0900

    11047 필요한 동전의 최소 개수 세는 문제

---
+++ 11047.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+bool c(int a, int b)
+{
+	return a > b;
+}
+int main()
+{
+	vector<int> v;
+	int N, price, num;
+	cin >> N >> price;
+	for (int i = 0; i < N; i++)
+	{
+		cin >> num;
+		v.push_back(num);
+	}
+
+	sort(v.begin(), v.end(), c);
+	
+	int i = 0;
+	int count = 0;
+	while (true)
+	{
+		if (v[i] > price)
+			i++;
+		else
+		{
+			price -= v[i];
+			count++;
+		}
+		if (price == 0)
+			break;
+	}
+	cout << count << endl;
+	
+}
